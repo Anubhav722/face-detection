52f3649fd75b5ff1bde21a4f511b46a538157682
Anubhav722 <anubhavs286@gmail.com>
2018-01-27 16:55:17 +0530

    importing the cascades

---
+++ open_files/face_detection.py
@@ -0,0 +1,8 @@
+import cv2
+
+
+# Loading the cascades. xml files
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
