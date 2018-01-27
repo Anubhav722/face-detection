b162be4ea23e41517467d7b883bd4e12213ed6a0
Anubhav722 <anubhavs286@gmail.com>
2018-01-27 23:06:52 +0530

    add smile detection snippet

---
+++ open_files/smile_detection.py
@@ -0,0 +1,74 @@
+import cv2
+
+
+# Loading the cascades. xml files
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Define functions that will do the detections.
+
+# will take input of images coming from the webcam.
+# NOTE: Cascades work o black and white image.
+# So accepting the image here in black and white (gray)
+# and also the original image
+def detect(gray, frame):
+	# Get the coordinates of the rectangle which detect the face.
+
+	# x, y are the coordiantes on the upper left corner.
+	# w and h are the width and height respectively.
+	# x, y, w, h = 
+
+	# detectMultiScale method takes the gray image.and 
+	# other arguments include the factor by which image size is going to be reduced.
+	# min. no. neighbours.
+
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	# print (faces) # tuples (x,y,w,h)
+
+	for (x,y,w,h) in faces:
+		# Draw the rectangle for the faces.
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+		# Eyes will be detected where we have found our face.
+		# So we will perform our tests on the face's referential frame in order to save the computational time.
+
+		# We will be taking to region of interest. The gray scale image and the colored image.
+
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+w]
+
+		# Detecting the eyes.
+		eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+
+		for (ex, ey, ew, eh) in eyes:
+			cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+
+		# Smile detector.
+		smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+
+		for (sx, sy, sw, sh) in smile:
+			cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0,0,255), 2)
+
+	return frame
+
+# DO face recognition using webcam.
+# We will apply this function to the last frame coming on the webcam.
+
+video_capture = cv2.VideoCapture(0) # 0 for computer webcam.
+
+while True:
+	_, last_frame = video_capture.read()
+	# convert to gray scale
+	gray = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+
+	canvas = detect(gray, last_frame)
+
+	cv2.imshow('Video', canvas)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+video_capture.release()
+cv2.destroyAllWindows()
