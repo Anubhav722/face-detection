06ee054036be8ed29d4910ba6ac9aa7e31c02241
sharmparth1993 <sharmaparth17@gmail.com>
2018-01-30 03:33:35 +0530

    add transformations to the frame

--- ssd_object_detection/object_detection.py
+++ ssd_object_detection/object_detection.py
@@ -22,4 +22,28 @@ def detect(frame, net, transform):
     # black and white image has channel 1. and the colored image has channel as 3 (BGR).
     
     # Perform a series of transformations to frame to get a torch variable.
+    # Or series of transformations to make before feeding into the neural network
     
+
+    # First transformation.
+    frame_t = transform(frame)[:0]
+    # returns frame in the right format in the form of numpy array
+    
+    # Second transformation
+    # Converting the numpy array into the torch tensor.
+    # Torch tensor is a much more advanced matrix than the numpy array.
+    x = torch.from_numpy(frame_t).permute(2, 0, 1)
+    # It is converting the sequence red, blue, green into the sequence green, red, blue.
+
+    
+    # Third transformation
+    # Add this fake dimension corresponding to the batch.
+    # Neural network cannot accept the single input vector or single input image.
+    # Neural network only accepts batches of inputs.
+    # x.unsqueeze(0)
+
+    
+    # Fourth transformation
+    # Converting the torch sensor function to torch variable.
+    # Torch variable is a highly advanced variable which contains both a tensor and gradient.
+    x = Variable(x.unsqueeze(0))
